c324af6657a09c0f560eb38b7f1474022ee219ce
pschlo <>
2023-07-04 11:20:16 +0200

    started building own classes for group and round

--- src/fetch_allocations.py
+++ src/fetch_allocations.py
@@ -5,6 +5,8 @@ from itertools import chain
 import json
 import time
 from pathlib import Path
+# from collections.abc import Sequence
+from typing import Any, TypeVar
 
 SCRIPT_DIR = Path(__file__).parent
 
@@ -44,16 +46,62 @@ class AllocationError(ValueError):
     pass
 
 
-class Allocation:
-    rounds: list[list[list[int]]]
-    num_rounds: int
+class Group(list[int]):
+    def __init__(self, people: set[int]):
+        super().__init__(people)
+        
+        # check that no duplicate people
+        assert len(set(people)) == len(people)
+
+        # sort people from smallest to largest
+        self.sort()
+
+
+class Round(list[Group]):
+    def __init__(self, people: set[Any], groups: set[Group]):
+        super().__init__(groups)
+        self.people = people
+        self.num_people = len(people)
+
+        # sort groups from largest to smallest
+        self.sort(key=len, reverse=True)
+
+        # group sizes are sorted from largest to smallest
+        group_sizes = Counter(sorted(map(len, self), reverse=True)).items()
+        self.group_sizes = [(num, size) for size, num in group_sizes]
+        
+        # check that each person appears exactly once
+        if not Counter(chain(*self)) == Counter(people):
+            return False
+
+
+
+class Allocation(list[Round]):
     num_people: int
     num_groups: int
     group_sizes: list[tuple[int,int]]
 
-    def __init__(self, rounds: list[list[list[int]]]) -> None:
-        self.rounds = rounds
-        self.num_rounds = len(rounds)
+    def __init__(self, rounds: list[Round]):
+        super().__init__(rounds)
+        self.num_people = self[0].num_people
+
+
+        # check that all rounds have the same group sizes
+        if not all(round.group_sizes == self[0].group_sizes for round in self):
+            return False
+
+        # check if actual solution, i.e. that noone is in a group with another person more than once
+        seen: dict[int, set[int]] = {p: set() for p in range(1, self.num_people+1)}
+        for round in self:
+            for group in round:
+                group = set(group)
+                for person in group:
+                    others = group - {person}
+                    # check intersection
+                    if seen[person] & others:
+                        return False
+                    seen[person].update(others)
+
 
         # convert person symbols to person numbers, which start at 1
         symbols = sorted({x for round in rounds for group in round for x in group})
@@ -62,15 +110,8 @@ class Allocation:
 
         # in each round, sort groups from largest to smallest
         # in each group, sort person from smallest to largest
-        for round in rounds:
-            round.sort(key=len, reverse=True)
-            for i in range(len(round)):
-                round[i] = sorted([symbol_to_person[symbol] for symbol in round[i]])
+        # round[i] = sorted([symbol_to_person[symbol] for symbol in round[i]])
 
-        self.num_groups = len(rounds[0])
-        
-        group_sizes = Counter(sorted(map(len, rounds[0]), reverse=True)).items()
-        self.group_sizes = [(num, size) for size, num in group_sizes]
 
         # set title
         groups_str = ', '.join(f"{num} groups of {size}" for num, size in self.group_sizes)
@@ -95,30 +136,13 @@ def is_valid(allocation: Allocation) -> bool:
     a = allocation
 
     print("here0")
-    # check that each person appears in each round exactly once
-    unique_people = Counter(range(1, a.num_people+1))
-    if not all(Counter(chain(*round)) == unique_people for round in a.rounds):
-        return False
+    
     
     print("here1")
 
-    # check that all rounds have the same group sizes
-    group_sizes = [Counter(map(len, round)) for round in a.rounds]
-    if not all(sizes == group_sizes[0] for sizes in group_sizes):
-        return False
     
     print("here2")
-    # check if actual solution, i.e. that noone is in a group with another person more than once
-    seen: dict[int, set[int]] = {p: set() for p in range(1, a.num_people+1)}
-    for round in a.rounds:
-        for group in round:
-            group = set(group)
-            for person in group:
-                others = group - {person}
-                # check intersection
-                if seen[person] & others:
-                    return False
-                seen[person].update(others)
+
     
     return True
 
